2d269a88808a028804063617921e6bbf20d78ec0
Hardik Shah <102228659+Hardikk013@users.noreply.github.com>
2026-08-12 12:47:52 +0530

    Add Array insertion program

---
+++ arrayinsertion.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int s,index,value;
+
+    cout<<"Enter The Size Of An Array : ";
+    cin>>s;
+
+    int enro[s];
+
+    cout<<"Enter Your Array Elements : ";
+
+    for(int i=0 ; i<s ; i++){
+        cin>>enro[i];
+    }
+
+    for(int i=0 ; i<s ; i++){
+        cout<<enro[i]<<" ";
+    }
+
+    cout<<" "<<endl;
+
+    cout<<"Enter The Insertion Index : ";
+    cin>>index;
+
+    cout<<"Enter New Value : ";
+    cin>>value;
+
+    for(int i=s ; i>index ; i--){
+        enro[i] = enro[i-1];
+    }
+
+    enro[index] = value;
+
+    for(int i=0 ; i<s ; i++){
+        cout<<enro[i]<<" ";
+    }
+}
